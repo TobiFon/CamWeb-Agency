c68919da4b75249a5650e4b8ce2c3b871c9f74fc
Tobi Fon <tobifon98@gmail.com>
2023-12-26 12:28:24 +0100

    little change

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,8 +6,19 @@ import ReviewSection from "./ReviewSection";
 import ServicesSection from "./ServicesSection";
 import WhySection from "./WhySection";
 import ContactSection from "./ContactSection";
+import Lenis from "@studio-freight/lenis";
 
 const HomePage = () => {
+  if (typeof window !== "undefined") {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }
+
   return (
     <div>
       <HeroSection />
